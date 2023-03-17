0f7f84c25b9ae9a1ea81aee8bead3142bbecf64a
Estudiante USC <estudiante@GTACAD.USC>
2023-03-16 20:04:41 -0500

    parcial1

--- clase 5 - ejercicio/ejercicio.py
+++ clase 5 - ejercicio/ejercicio.py
@@ -1,12 +1,13 @@
 from cargardatos import *
 
+
 archivo = input("Nombre del archivo de datos?: ")
 load_csv_py(archivo)
 
-def camu (comu):
-    return "atraco" > 1000
+#def camu (comu):
+ #   return  > 10
 
-comu_filtrada = list(filter(camu, archivo))
+#comu_filtrada = list(filter(camu, archivo))
 
-#comu_filtrada = list(filter(lambda x: x["atraco"] > 1000, archivo))
+comu_filtrada = list(filter(lambda x: x["atraco"] > 1000, archivo))
 print(comu_filtrada)
