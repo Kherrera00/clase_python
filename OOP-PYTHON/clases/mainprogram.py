6251a173204511c6aafef8d7182cb20286464634
Estudiante USC <estudiante@GTACAD.USC>
2023-03-30 20:40:42 -0500

    clase interfacez

--- OOP-PYTHON/clases/mainprogram.py
+++ OOP-PYTHON/clases/mainprogram.py
@@ -2,8 +2,16 @@ from clases1 import *
 
 #objeto
 
-Diego = persona("Diego", "888")
-Diana = persona("Diana", "999")
+# Diego = persona("Diego", "888")
+# Diana = persona("Diana", "999")
 
-Diego.printInfo()
-Diana.printInfo()
+# Diego.printInfo()
+# Diana.printInfo()
+
+sesion1 = Session()
+
+s2 = Session.getInstance()
+s3 = Session.getInstance()
+
+print(s2)
+print(s3)
